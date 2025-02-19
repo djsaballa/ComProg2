2f9ded047ebea1e192ccaf285912b80d2a147e01
djsaballa <71679357+djsaballa@users.noreply.github.com>
2025-02-18 22:56:39 -0800

    Update SwitchCaseDemo.java

--- SwitchCaseDemo.java
+++ SwitchCaseDemo.java
@@ -1,17 +1,17 @@
 public class SwitchCaseDemo {
 	public static void main(String[] args) {
-
-	  int num = 9;
-	  
-	  switch(num) {  // Check expression in switch condition
-  	  case 9:  // Output differs depending on num variable value
-  		  System.out.println("Num is 9");
-  		  break;
-  	  case 10:
-  		  System.out.println("Num is 10");
-  		  break;
-  	  default:
-  		  System.out.println("Num is not 9 or 10");
-	  }
-  }
+	
+		int num = 9;
+		
+		switch(num) {  // Check expression in switch condition
+			case 9:  // Output differs depending on num variable value
+				System.out.println("Num is 9");
+				break;
+			case 10:
+				System.out.println("Num is 10");
+				break;
+			default:
+				System.out.println("Num is not 9 or 10");
+		}
+	}
 }
