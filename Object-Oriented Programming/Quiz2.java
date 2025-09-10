0b1108096943f275545728f86d3b92d5ba630226
djsaballa <71679357+djsaballa@users.noreply.github.com>
2025-09-10 10:34:56 +0800

    Update Quiz2.java

--- Object-Oriented Programming/Quiz2.java
+++ Object-Oriented Programming/Quiz2.java
@@ -10,37 +10,37 @@ public class Quiz1 {
 			System.out.println("i dont like oop :( ");
 		}
 
-    int day = 2; 
+	    int day = 2; 
+	
+	    switch (day) {
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			default:
+				System.out.println("Invalid day");
+	    }
 
-    switch (day) {
-        case 1:
-            System.out.println("Monday");
-            break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        case 3:
-            System.out.println("Wednesday");
-            break;
-        default:
-            System.out.println("Invalid day");
-    }
-
-   int score = 85; 
-
-    if (score >= 90) {
-        System.out.println("Grade: A");
-    } else if (score >= 80) {
-        System.out.println("Grade: B");
-    } else if (score >= 70) {
-        System.out.println("Grade: C");
-    } else if (score >= 60) {
-        System.out.println("Grade: D");
-    } else {
-        System.out.println("Grade: F");
-    }
-
-    System.out.println("i LOVE oop <3");
+	   int score = 85; 
+	
+	    if (score >= 90) {
+	        System.out.println("Grade: A");
+	    } else if (score >= 80) {
+	        System.out.println("Grade: B");
+	    } else if (score >= 70) {
+	        System.out.println("Grade: C");
+	    } else if (score >= 60) {
+	        System.out.println("Grade: D");
+	    } else {
+	        System.out.println("Grade: F");
+	    }
+	
+	    System.out.println("i LOVE oop <3");
 
 	}
 
